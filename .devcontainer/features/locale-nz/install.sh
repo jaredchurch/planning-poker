6babac92efb8b514f4556abc6c90de5537e80abb
Jared Church <pjaredchurch@gmail.com>
2026-06-22 20:55:46 +1200

    update devcontainer

--- .devcontainer/features/locale-nz/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-sudo sed -i '/en_NZ.UTF-8/s/^# //' /etc/locale.gen
-sudo locale-gen en_NZ.UTF-8
-echo "✅ NZ Locale Install Done"
-
-### End of File
--- .devcontainer/features/spec-kit/install.sh
+++ .devcontainer/features/spec-kit/install.sh
@@ -3,6 +3,13 @@ set -e
 
 # refer: https://github.com/github/spec-kit
 
+# export DEBIAN_FRONTEND=noninteractive
+
+echo "################ Pip Update"
+python3 -m pip install --upgrade pip
+
+pip install uv
+
 uv tool install specify-cli --from git+https://github.com/github/spec-kit.git
 uv tool update-shell
 echo "✅ Install Spec Kit"
